77c8457b8fa1ae36207f5a46638e93322aa35e0c
Adam <adamdiallo2k@gmail.com>
2023-08-22 14:12:44 -0700

    fix pepe-8

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
 """
-This module starts a
-Flask web application with the following specifications:
+This module starts a Flask web application with the following specifications.
 """
 
 import csv
 import requests
 import sys
 
+
 def get_employee_todo_progress(employee_id):
-    """display the API information"""
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    user_data = requests.get(url).json()
-    tasks = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}').json()
+    """Display the API information."""
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = f"{base_url}/users/{employee_id}"
+    todos_url = f"{base_url}/todos?userId={employee_id}"
+
+    user_data = requests.get(user_url).json()
+    tasks = requests.get(todos_url).json()
 
     with open(f'{employee_id}.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in tasks:
-            writer.writerow([employee_id, user_data['username'], str(task['completed']), task['title']])
+            writer.writerow([employee_id, user_data['username'],
+                             str(task['completed']), task['title']])
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
