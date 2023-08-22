8992ba376e66bae309719aec48323089159a9be3
Adam <adamdiallo2k@gmail.com>
2023-08-22 09:43:45 -0700

    edit pep-8 eror

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,10 +7,13 @@ Flask web application with the following specifications:
 import requests
 import sys
 
+
 def get_employee_todo_progress(employee_id):
     """display the API information"""
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    user_url = f"https://jsonplaceholder.typicode.com/
+    users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/
+    users/{employee_id}/todos"
 
     user_response = requests.get(user_url)
     todos_response = requests.get(todos_url)
@@ -22,7 +25,8 @@ def get_employee_todo_progress(employee_id):
     user_data = user_response.json()
     todos_data = todos_response.json()
 
-    completed_tasks = [task for task in todos_data if task['completed']]
+    completed_tasks = [task for task in todos_data 
+    if task['completed']]
     total_tasks = len(todos_data)
 
     print(f"Employee {user_data['name']} is done with tasks({len(completed_tasks)}/{total_tasks}):")
@@ -38,4 +42,3 @@ if __name__ == "__main__":
             get_employee_todo_progress(employee_id)
         except ValueError:
             print("Please provide a valid integer as the employee ID.")
-
