11afae764d24afc7614cc032259578d2861877ac
Adam <adamdiallo2k@gmail.com>
2023-08-22 10:45:15 -0700

    test

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -4,11 +4,9 @@ This module starts a
 Flask web application with the following specifications:
 """
 
-
 import csv
 import requests
 import sys
-
 
 def get_employee_todo_progress(employee_id):
     """display the API information"""
@@ -19,8 +17,7 @@ def get_employee_todo_progress(employee_id):
     with open(f'{employee_id}.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in tasks:
-            writer.writerow([employee_id, user_data['name'], task['completed'], task['title']])
-
+            writer.writerow([employee_id, user_data['username'], str(task['completed']), task['title']])
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
